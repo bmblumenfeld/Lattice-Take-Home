c8d259416fde2a51afa1b796c82bf5a2c158d876
Ben Blumenfeld <ben.blumenfeld@myagi.com>
2020-03-04 18:33:09 -0800

    sorted out movieDb

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
-const getRecentPopularMovies = require("./utils.js");
+const { getPopularMovies, getMoviesFromSearch } = require("./utils.js");
+
+const INTERNAL_SERVER_ERROR_STATUS = "500";
 
 const app = express();
 
@@ -10,5 +12,21 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 app.get("/popular", async (req, res) => {
-  await getRecentPopularMovies();
+  try {
+    const allTimePopularMovies = await getPopularMovies();
+    res.send(allTimePopularMovies);
+  } catch (error) {
+    res.sendStatus(INTERNAL_SERVER_ERROR_STATUS);
+    throw error;
+  }
+});
+
+app.get("/search", async (req, res) => {
+  try {
+    const searchResults = await getMoviesFromSearch("nice");
+    res.send(searchResults);
+  } catch (error) {
+    res.sendStatus(INTERNAL_SERVER_ERROR_STATUS);
+    throw error;
+  }
 });
--- backend/utils.js
+++ backend/utils.js
@@ -1,15 +1,37 @@
 const fetch = require("node-fetch");
 const { MOVIE_DB_API_KEY } = require("./config.js");
 
-const popularQuery = `
-https://api.themoviedb.org/3/discover/movie?api_key=${MOVIE_DB_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1
-
-`;
-
-const getRecentPopularMovies = async () => {
-  fetch(popularQuery).then(response => {
-    console.log(response);
-  });
+const getPopularMovies = async (pageNumber = 1) => {
+  const popularQuery = `https://api.themoviedb.org/3/discover/movie?api_key=${MOVIE_DB_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageNumber}`;
+  try {
+    const response = await fetch(popularQuery);
+    if (!response.ok) {
+      throw "Bad request to MovieDB";
+    }
+    const resBody = await response.json();
+    const popularMovies = resBody.results;
+    return popularMovies;
+  } catch (error) {
+    throw error;
+  }
 };
 
-module.exports = getRecentPopularMovies;
+const getMoviesFromSearch = async (searchTerm, pageNumber = 1) => {
+  const searchQuery = `https://api.themoviedb.org/3/search/movie?api_key=28bd402a0e9e028a11e0076c5f8f3d86&language=en-US&query=${searchTerm}&page=${pageNumber}&include_adult=false`;
+  try {
+    const response = await fetch(searchQuery);
+    if (!response.ok) {
+      throw "Bad request to MovieDB";
+    }
+    const resBody = await response.json();
+    const searchResults = resBody.results;
+    return searchResults;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  getPopularMovies,
+  getMoviesFromSearch
+};
